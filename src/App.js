f53d4dc1f316a81798c369f41ef8d67d11376b66
piboyeux <ethan.piboyeux@mmibordeaux.com>
2020-11-09 16:30:19 +0100

    pull

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ class App extends Component {
 
         <Main_app />
 
+
       </div>
 
     );
--- src/components/connexion/Connexion.js
+++ src/components/connexion/Connexion.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Connexion.css';
 import logo_blanc_podkast from '../img/logo_blanc_podkast.svg';
+import Btn_bf_gg from '../btn_fb_gg/Btn_fb_gg';
 
 class Connexion extends Component {
   render () {
@@ -11,14 +12,7 @@ class Connexion extends Component {
             <img className="logo" src={logo_blanc_podkast} alt="logo podkast" />
             <div>
                 <h1 className="color_white_connexion text_align_center"> Connectez-vous ! </h1>
-                <div className="flex_connexion">
-                    <div className="fle_btn_facebook_logo">
-                      <button className="bouton_connexion_fb_gg">Facebook</button>
-                    </div>
-                    <div className="le_btn_facebook_logo">
-                      <button className="bouton_connexion_fb_gg">Google</button>
-                    </div>
-                </div>
+                  <Btn_bf_gg />
                 <div className="input_style_connexion">
                   <input type="text" className="input_connexion" placeholder="Email" ></input>
                   <input type="text" className="input_connexion" placeholder="Mot de passe"></input>
