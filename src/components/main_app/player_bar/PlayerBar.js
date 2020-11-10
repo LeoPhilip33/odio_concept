82849a7bb0fe7954edc42efeeb7becae883b66b2
piboyeux <ethan.piboyeux@mmibordeaux.com>
2020-11-10 12:31:05 +0100

    player bar

--- src/components/main_app/player_bar/PlayerBar.js
+++ src/components/main_app/player_bar/PlayerBar.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
 import './PlayerBar.css';
-import
+import like from '../../img/like.svg';
+import dislike from '../../img/dislike.svg';
+import bookmark from '../../img/bookmark.svg';
+import chromecast from '../../img/chromecast.svg';
+import volume_control from '../../img/volume_control.svg';
+import volume_up from '../../img/volume_up.svg';
+
+
+
 
 class PlayerBar extends Component {
   render () {
     return (
         <div className="container_player_bar">
+
             <div className="info_lecture">
                 <div>Titre du podcast</div>
                 <div>Nom</div>
-                <img className="like_dislike" src={like}/>
-                <img className="like_dislike" src={dislike}/>
+                    <div className="like_dislike_align">
+                        <img className="like_dislike" src={like}/>
+                        <img className="like_dislike" src={dislike}/>
+                    </div>
+                
+            </div>
+
+            <div className="bar_lecture">
+                
+            </div>
+
+            <div className="icon_setting">
+                <img className="icon_setting_bookmark" src={bookmark}/>
+                <img className="icon_setting_chromecast" src={chromecast}/>
+                <img className="icon_setting_volume_up" src={volume_up}/>
+                <img className="icon_setting_volume_control" src={volume_control}/>
             </div>
         </div>
        
