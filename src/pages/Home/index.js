d9ce85e3f27927469c2eaafa40513605d8cfee83
piboyeux <ethan.piboyeux@mmibordeaux.com>
2020-11-12 12:43:08 +0100

    pull leo

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import MainAPP from '../../components/main_app/Main_app';
+import MainAPP from '../main_app/Main_app';
 
 const HomePage = () => {
     return (
