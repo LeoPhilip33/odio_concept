1742f8399e2306d8f393302ac275f49812bf2e8f
PHILIP Léo <leo.philip33.bordeaux@gmail.com>
2020-11-12 16:15:22 +0100

    push

---
+++ src/components/parametres/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './parametres.css';
+
+class Parametres extends Component {
+  render () {
+    return (
+        <div className="container_parametres">
+
+            <div>
+                etzet
+            </div>
+
+        </div>
+       
+    );
+  }
+}
+
+export default Parametres;
--- src/pages/parametres/index.js
+++ src/pages/parametres/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Main_APP from '../main_app/Main_app'
+import Parametre from '../../components/parametres/index'
 
 const ParametreComptePage = () => {
     return (
         <div> 
-            Paramètres
+            <Main_APP />
+            <h1></h1>
+            <Parametre />
         </div> 
     )
 }
