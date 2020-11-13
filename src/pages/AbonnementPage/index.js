870f1f55e903f475e9a9c2fdddc6bf313db1358a
PHILIP Léo <leo.philip33.bordeaux@gmail.com>
2020-11-13 11:41:35 +0100

    fix all push les offres pages

--- src/pages/AbonnementPage/index.js
+++ src/pages/AbonnementPage/index.js
@@ -18,61 +18,66 @@ const AbonnementPage = () => {
                             <img className="arrow_return" src={Return} alt=""/>
                             <div className="retour">Retour</div>
                         </div>
-                        <h1 className="style_title">Les offres</h1>
+                        <h1 className="style_title_abonnementPage">Les offres</h1>
                     </div>
                     <div className="disposition_cadre">
                         <div className="cadre">
                             <h1>FREE</h1>
-                               <div className="barre"><div className="gradian_barre"></div></div>
-                               <form class="style_form">
-                                    <CheckboxVide text="Qualité Supérieure"/>
-                                    <CheckboxVide text="Qualité Flac"/>
-                                    <CheckboxVide text="Téléchargements"/>
-                                    <CheckboxVide text="Accès aux lives"/>
-                                    <CheckboxVide text="Créer son live"/>
-                                    <CheckboxVide text="Marque-Page"/>
-                                    <CheckboxVide text="Pas de pubs"/>
-                                    <CheckboxVide text="Monétiser ses podcasts"/>
-                                </form>
-                                   <p className="price_bold" >0€/mois</p>
-                                   <Button>S'ABONNER</Button>
-
-                        
+                            <div className="barre"><div className="gradian_barre"></div></div>
+                            <form class="style_form">
+                                <CheckboxVide text="Qualité Supérieure"/>
+                                <CheckboxVide text="Qualité Flac"/>
+                                <CheckboxVide text="Téléchargements"/>
+                                <CheckboxVide text="Accès aux lives"/>
+                                <CheckboxVide text="Créer son live"/>
+                                <CheckboxVide text="Marque-Page"/>
+                                <CheckboxVide text="Pas de pubs"/>
+                                <CheckboxVide text="Monétiser ses podcasts"/>
+                            </form>
+                            <p className="price_bold" >0€/mois</p>
+                            <div className="container_btnAbonnementPageAbonne">
+                                <div className="btnAbonnementPageAbonne"> S'ABONNER </div>
+                            </div>                       
                         </div>
                         <div className="cadre">
                             <h1>PREMIUM</h1>
                               <div className="barre"><div className="gradian_barre"></div></div>
                                 <form class="style_form">
-                                        <CheckboxRemplis text="Qualité Supérieure"/>
-                                        <CheckboxVide text="Qualité Flac"/>
-                                        <CheckboxRemplis text="Téléchargements"/>
-                                        <CheckboxRemplis text="Accès aux lives"/>
-                                        <CheckboxVide text="Créer son live"/>
-                                        <CheckboxRemplis text="Marque-Page"/>
-                                        <CheckboxRemplis text="Pas de pubs"/>
-                                        <CheckboxVide text="Monétiser ses podcasts"/>
-                                    </form> 
+                                    <CheckboxRemplis text="Qualité Supérieure"/>
+                                    <CheckboxVide text="Qualité Flac"/>
+                                    <CheckboxRemplis text="Téléchargements"/>
+                                    <CheckboxRemplis text="Accès aux lives"/>
+                                    <CheckboxVide text="Créer son live"/>
+                                    <CheckboxRemplis text="Marque-Page"/>
+                                    <CheckboxRemplis text="Pas de pubs"/>
+                                    <CheckboxVide text="Monétiser ses podcasts"/>
+                                </form> 
                               <p className="price_bold" >10€/mois</p>
-                              <div></div>
-                              <Button>S'ABONNER</Button>
+                              <div className="container_btnAbonnementPageAbonne">
+                                <div className="btnAbonnementPageAbonne"> S'ABONNER </div>
+                              </div>
                         </div>
                         <div className="cadre">
                             <h1>PROFESSIONELLE</h1>
-                                <div className="barre"><div className="gradian_barre"></div></div>
-                                <div>
-                                    <form class="style_form">
-                                        <CheckboxRemplis text="Qualité Supérieure"/>
-                                        <CheckboxRemplis text="Qualité Flac"/>
-                                        <CheckboxRemplis text="Téléchargements"/>
-                                        <CheckboxRemplis text="Accès aux lives"/>
-                                        <CheckboxRemplis text="Créer son live"/>
-                                        <CheckboxRemplis text="Marque-Page"/>
-                                        <CheckboxRemplis text="Pas de pubs"/>
-                                        <CheckboxRemplis text="Monétiser ses podcasts"/>
-                                    </form>
-                                    <p className="price_bold" >20€/mois</p>
-                                    <Button>S'ABONNER</Button>
+                            <div className="barre">
+                                <div className="gradian_barre"></div>
+                            </div>
+                            <div>
+                                <form class="style_form">
+                                    <CheckboxRemplis text="Qualité Supérieure"/>
+                                    <CheckboxRemplis text="Qualité Flac"/>
+                                    <CheckboxRemplis text="Téléchargements"/>
+                                    <CheckboxRemplis text="Accès aux lives"/>
+                                    <CheckboxRemplis text="Créer son live"/>
+                                    <CheckboxRemplis text="Marque-Page"/>
+                                    <CheckboxRemplis text="Pas de pubs"/>
+                                    <CheckboxRemplis text="Monétiser ses podcasts"/>
+                                </form>
+                                <p className="price_bold" >20€/mois</p>
+                                <div className="container_btnAbonnementPageAbonne">
+                                    <div className="btnAbonnementPageAbonne"> S'ABONNER </div>
                                 </div>
+                            </div>
                         </div>
                     </div>
                 </div>
